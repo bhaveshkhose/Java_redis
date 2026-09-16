2379e0fd6957684f5be039323b10fb977e012a93
bhaveshkhose <khosebhavesh@gmail.com>
2026-09-16 20:05:49 +0530

    changes in data store class

--- src/main/java/org/redis/RedisStore.java
+++ src/main/java/org/redis/RedisStore.java
@@ -1,4 +1,26 @@
 package org.redis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RedisStore {
+
+    private final Map<String , String> data = new HashMap<>();
+
+
+    public String set(String key , String value){
+        return data.put(key , value);
+    }
+
+    public String get(String key){
+        return data.get(key);
+    }
+
+    public boolean exists(String key){
+        return data.containsKey(key);
+    }
+
+    public boolean delete(String key){
+        return data.remove(key) != null ;
+    }
 }
